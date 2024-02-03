a76bfb555e79708a00788e88937e31a696c3e362
menso431 <menson.sundash@gmail.com>
2024-02-03 15:28:24 +1100

    Functions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-//Arrays
-let selectedProducts = []; //Array literals; indicating empty array
+// Function
+//define function
+function greet(name, lastName) {// name is parameter
+    //body of function
+    console.log(`Hello ${name} ${lastName}, Welcome!`);
+}
 
-selectedProducts = ['Chairs', 'Tables', 'dinning set'];
-selectedProducts[3] = 'Shoes Racks' 
-console.log(selectedProducts.length);
+//function call
+greet('John', 'Coel'); //function with argument
+greet('Marry', 'Moose');
