4e991394881f4dbc830f7b8f4b4fea0958183b11
menso431 <menson.sundash@gmail.com>
2024-02-03 16:56:15 +1100

    Constructor Function

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 //Objects
 //Declaring variable using Var has issues like scoping so use either let or Const
 //ES6
-const circle = {//{}object literals syntax 
-    radius:1, //properties
-    location: { //properties
-        x: 1,
-        y: 1
-    },
-    draw: function () { //methods
+
+//Constructor Function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
         console.log('draw');
     }
-};
+}
 
-
-circle.draw();
+const another = new Circle(1);
+//when we use new operator to call a function
+// it will create empty object ,(Circle {})
+// Then it will set this to point to that object
+// Finally it will return that object from this(Circle()) function
