c1f412bdd10639d7d98789d6146c7c72b8e3ef31
menso431 <menson.sundash@gmail.com>
2024-02-04 23:06:25 +1100

    Getters and Setters

--- index.js
+++ index.js
@@ -11,17 +11,29 @@ function Circle(radius) {
 
     let defaultLocation = {x: 0, y: 0};
 
-    let computeOptimumLocation = function(factor) {
-
+    //if you want to get the private function outside scope of the class
+    //Getter
+    this.getDefaultLocation = function() {
+        return defaultLocation;
     }
+
     this.draw = function () {
-        computeOptimumLocation(0.1);
-        // defaultLocation
-        //this.radius
         console.log('draw');
     }
+
+    //Use Object.defineProperty to define getter or setter
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation;
+        },
+        set: function(value) {
+            if(!value.x || !value.y)
+                throw new Error('Invalid Location');
+            defaultLocation = value;
+        }
+    })
 }
 
 const circle = new Circle(10);
-
+circle.defaultLocation = 1;
 circle.draw();
