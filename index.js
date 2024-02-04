8391c3757e73c3536aba87d21e02988b1e6236be
menso431 <menson.sundash@gmail.com>
2024-02-04 22:48:57 +1100

    Abstraction, Private properties and Methods

--- index.js
+++ index.js
@@ -4,26 +4,24 @@
 //Declaring variable using Var has issues like scoping so use either let or Const
 //ES6
 
-//Enumerating properties
+// Abstraction
 function Circle(radius) {
+    
     this.radius = radius;
+
+    let defaultLocation = {x: 0, y: 0};
+
+    let computeOptimumLocation = function(factor) {
+
+    }
     this.draw = function () {
+        computeOptimumLocation(0.1);
+        // defaultLocation
+        //this.radius
         console.log('draw');
     }
 }
 
 const circle = new Circle(10);
-//Enumerating all the members of an object
-for(let key in circle) {
-    if(typeof circle[key] !== 'function')
-        console.log(key, circle[key])
-}
 
-//another approach
-//to get all the keys in an Object use Object.keys
-const keys = Object.keys(circle)
-console.log(keys);
-
-//To check for existing of a property or a method in an object use in operator
-if('radius' in circle)
-    console.log('Circle has a radius')
+circle.draw();
