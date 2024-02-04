b4653247d6ad6ba4e0d09b58ef0c8ec8721f9adf
menso431 <menson.sundash@gmail.com>
2024-02-04 22:32:00 +1100

    Enumerating Properties

--- index.js
+++ index.js
@@ -4,11 +4,26 @@
 //Declaring variable using Var has issues like scoping so use either let or Const
 //ES6
 
-//Primitives Vs Reference
-let obj = { value: 10 };
-function increase(obj) {
-    obj.value++;
+//Enumerating properties
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
 }
 
-increase(obj);
-console.log(obj);
+const circle = new Circle(10);
+//Enumerating all the members of an object
+for(let key in circle) {
+    if(typeof circle[key] !== 'function')
+        console.log(key, circle[key])
+}
+
+//another approach
+//to get all the keys in an Object use Object.keys
+const keys = Object.keys(circle)
+console.log(keys);
+
+//To check for existing of a property or a method in an object use in operator
+if('radius' in circle)
+    console.log('Circle has a radius')
